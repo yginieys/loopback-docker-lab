c7125fd2ed59cb487644493b866aa7e43beab962
yginieys <yginieys@stepnet.fr>
2017-09-08 16:34:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# YG. Essai de dockerization de loopback
+
+Build l'image
+docker run --name lb -p 3000:3000 -d yginieys/loopback
+
+
+
+# loopback-getting-started-intermediate
+
+Please see LoopBack [Getting started part II](http://loopback.io/doc/en/lb3/Getting-started-part-II.html) for the tutorial that walks you through creating this application.
+
+```
+$ git clone https://github.com/strongloop/loopback-getting-started-intermediate.git
+$ cd loopback-getting-started-intermediate
+$ npm install
+$ node .
+```
+
+---
+
+[More LoopBack examples](https://loopback.io/doc/en/lb3/Tutorials-and-examples.html)
---
+++ server/server.js
@@ -0,0 +1,33 @@
+'use strict';
+
+console.log("1");
+var loopback = require('loopback');
+console.log("2");
+var boot = require('loopback-boot');
+console.log("3");
+
+var app = module.exports = loopback();
+console.log("4");
+
+app.start = function() {
+  // start the web server
+  return app.listen(function() {
+    app.emit('started');
+    var baseUrl = app.get('url').replace(/\/$/, '');
+    console.log('Web server listening at: %s', baseUrl);
+    if (app.get('loopback-component-explorer')) {
+      var explorerPath = app.get('loopback-component-explorer').mountPath;
+      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+    }
+  });
+};
+
+// Bootstrap the application, configure models, datasources and middleware.
+// Sub-apps like REST API are mounted via boot scripts.
+boot(app, __dirname, function(err) {
+  if (err) throw err;
+
+  // start the server if `$ node server.js`
+  if (require.main === module)
+    app.start();
+});
